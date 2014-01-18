9c493d86334e897ce5915a320e4552062776a7a3
mp42 <melpak92@yahoo.com>
2014-01-17 21:29:09 -0500

    removed redundant support

--- src/hashMapX/HashMapX.java
+++ src/hashMapX/HashMapX.java
@@ -6,12 +6,10 @@ import java.util.Stack;
 public class HashMapX {
 	
 	private HashMap<String,String> map_;
-	private HashMap<String,Integer> supportMap_;
 	private Stack<TransactionObj> activeXActions_;
 	
 	public HashMapX(){
 		map_ = new HashMap<>();
-		supportMap_ = new HashMap<>();
 		activeXActions_ = new Stack<>();
 	}
 	
@@ -41,10 +39,6 @@ public class HashMapX {
 			}
 			return;
 		}
-		if (parsedCommand[0].compareTo("NUMEQUALTO")==0){
-			System.out.println(this.getFreq(parsedCommand[1]));
-			return;
-		}
 		if (parsedCommand[0].compareTo("BEGIN")==0){
 			this.startXaction();
 			return;
@@ -68,25 +62,8 @@ public class HashMapX {
 		String old = null;
 		if (map_.containsKey(key)){
 			old = map_.get(key);
-			int oldFreq = supportMap_.get(old);
-			
-			// if new frequency is > 0 then update 
-			// else remove the value to optimize
-			// memory usage
-			if (oldFreq-1 >0){
-				supportMap_.put(old, oldFreq-1);
-			} else {
-				supportMap_.remove(old);
-			}
-			
 		} 
 		map_.put(key, value);
-		int freq = 1;
-		if (supportMap_.containsKey(value)){
-			freq = supportMap_.get(value);
-			freq++;
-		}
-		supportMap_.put(value, freq);
 		return old;
 	}
 	
@@ -95,30 +72,12 @@ public class HashMapX {
 		if (map_.containsKey(key)){
 			value = map_.get(key);
 			map_.remove(key);
-			int freq = supportMap_.get(value);
-			
-			// if the new frequency is > 0 then update it
-			// else remove the value from the HashMap to
-			// optimize memory usage
-			if (freq-1 > 0){
-				supportMap_.put(value, freq-1);
-			}
-			else {
-				supportMap_.remove(value);
-			}
 		}
 		return value;
 	}
 	
 	public String get(String key){
 		return map_.get(key);
-	}
-	
-	public int getFreq(String value){
-		if(supportMap_.containsKey(value)){
-			return supportMap_.get(value);
-		}
-		return 0;
 	}
 	
 	public void startXaction(){
